549ca778925bf9b503b08dbdded7e24fef533612
Omid Mafakher <Omid@mafakher.name>
2021-10-15 00:22:00 +0300

    Add project files.

---
+++ Xamarin.Android.Glide.Transformations/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Xamarin.Android.Glide.Transformations")]
+[assembly: AssemblyDescription("Binding for https://github.com/wasabeef/glide-transformations")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OmidID")]
+[assembly: AssemblyProduct("Xamarin.Android.Glide.Transformations")]
+[assembly: AssemblyCopyright("Copyright OmidID")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
